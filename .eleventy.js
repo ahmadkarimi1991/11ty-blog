5e05b9f4c6bacddc12d90f07a2fe5c763449cfa8
Ahmad Karimi <ahmadkarimi1991@gmail.com>
2022-05-12 14:38:22 +0430

    add date to blog and article

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,9 @@
-const { DateTime } = require('luxon');
-
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/main.css');
   eleventyConfig.addPassthroughCopy('./src/assets');
 
-  eleventyConfig.addFilter('date', date => {
-    return DateTime.fromISO(date).toFormat('LLLL dd, yyyy');
+  eleventyConfig.addFilter('postDate', (dateObj) => {
+    return new Date(dateObj).toLocaleDateString('fa-IR');
   });
 
   return {
